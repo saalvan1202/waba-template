0a54d6eee2ef172a4a079d14841ddf09bf901b77
Shande Alvan <ashande75@gmail.com>
2026-05-30 12:14:27 -0500

    --Cambiando pssq por dwfault

--- routers/template_public_router.py
+++ routers/template_public_router.py
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/api/v1/template-public", tags=["TEMPLATES PUBLIC"])
 @router.post("/security-code")
 #72580644-a24c-4862-8a06-29d9c1925617
 def security_code(data:SecurityCodeRequest):
-    if data.pssw != "72580644-a24c-4862-8a06-29d9c192561":
+    if data.pssw != "72580644-a24c-4862-8a06-29d9c1925617":
         return JSONResponse(content={"error": "Invalid password"}, status_code=400)
     # if data.telefono != "51965938082" and data.telefono != "51901981127":
     #     return JSONResponse(content={"error": "Número de teléfono no permitido"}, status_code=400)
